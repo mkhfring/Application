50c521efbb4ce3ade2132d1ad1a4abb4e11638bc
Mohamad <khajezade.mohamad@gmail.com>
2021-09-10 15:56:43 -0600

    Adding required fields as well as ragne

--- Dtos/CreateDto.cs
+++ Dtos/CreateDto.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 namespace ContactManagement.Dtos
 {
     public class CreateDto
     {
-        
+        [Required]
+        [Range(3, 50)]
         public string FirstName {set; get;}
+
+        [Required]
+        [Range(3, 50)]
         public string LastName{set; get;}
+        
         public string Email{set; get;}
         public int MobineNumber{set; get;}
     }
